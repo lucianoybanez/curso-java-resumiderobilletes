cd296fdcf5fc0e32405b33d357406e76c5e7a990
lucianoybanez@gmail.com <lucianoybanez@gmail.com@0f2bb4af-6c36-d3f4-e2bf-0f342f0e9b86>
2014-10-29 21:04:13 +0000

    add new entities

--- ResumideroBilletes/src/utn/resumiderobilletes/entidades/Movimiento.java
+++ ResumideroBilletes/src/utn/resumiderobilletes/entidades/Movimiento.java
@@ -6,13 +6,37 @@
 
 package utn.resumiderobilletes.entidades;
 
+import java.util.Objects;
+
 /**
  *
  * @author Luciano Ybañez
  */
 public class Movimiento {
+
+    public Movimiento(int numero, Cuenta cuenta, Categoria categoria) {
+        this.numero = numero;
+        this.cuenta = cuenta;
+        this.categoria = categoria;
+    }
+    
+    private final int numero;
+
+    public int getNumero() {
+        return numero;
+    }
     
     private Cuenta cuenta;
+    
+    private Categoria categoria;
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 
     public Cuenta getCuenta() {
         return cuenta;
@@ -20,6 +44,17 @@ public class Movimiento {
 
     public void setCuenta(Cuenta cuenta) {
         this.cuenta = cuenta;
+    }
+
+    
+    
+    
+    @Override
+    public boolean equals(Object o){
+        if (o instanceof Movimiento){
+            return ((Movimiento)o).getNumero() == this.getNumero();
+        }
+        return true;
     }
     
     
