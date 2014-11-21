42dad74ef9637d5c0406a67a6d36cc9eae9ded28
lucianoybanez <lucianoybanez@0f2bb4af-6c36-d3f4-e2bf-0f342f0e9b86>
2014-11-21 03:24:53 +0000

    Add altas a categorias

--- ResumideroBilletesUI/src/utn/resumiderobilletes/ui/forms/FrmPrincipal.java
+++ ResumideroBilletesUI/src/utn/resumiderobilletes/ui/forms/FrmPrincipal.java
@@ -6,6 +6,7 @@
 
 package utn.resumiderobilletes.ui.forms;
 
+import javax.swing.JOptionPane;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import utn.resumiderobilletes.entidades.Categoria;
@@ -38,32 +39,93 @@ public class FrmPrincipal extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         treeCategorias = new javax.swing.JTree();
+        txtCategoria = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                formWindowClosing(evt);
+            }
+        });
 
         treeCategorias.setName(""); // NOI18N
         jScrollPane1.setViewportView(treeCategorias);
         treeCategorias.getAccessibleContext().setAccessibleName("");
+
+        txtCategoria.setToolTipText("");
+        txtCategoria.setName("txtCategoria"); // NOI18N
+
+        jLabel1.setText("Nueva Categoria");
+
+        jButton1.setText("OK");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(25, 25, 25)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(582, Short.MAX_VALUE))
+                .addGap(23, 23, 23)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(txtCategoria, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton1)))
+                    .addComponent(jLabel1))
+                .addContainerGap(584, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(314, Short.MAX_VALUE))
+                .addGap(70, 70, 70)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtCategoria, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(221, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        boolean result = false;
+        if(!txtCategoria.getText().isEmpty()){             
+             ListaGenerica<Categoria> lista = Archivo.getDatos().getCategorias();
+             for (Categoria item : lista) {
+                if (item.getDescripcion().equals(txtCategoria.getText())){
+                    result = true;
+                }
+                 
+             }
+             if (result) {
+                     JOptionPane.showMessageDialog(this, "La categoria no puede ser igual a otra.", "Error", JOptionPane.ERROR_MESSAGE);
+                 }else{
+                     Categoria cat = new Categoria(lista.getCantidad() + 1, txtCategoria.getText());
+                     lista.agregar(cat);
+                     txtCategoria.setText("");
+                     cargarCategorias();
+                 }
+             
+        }else{
+             JOptionPane.showMessageDialog(this, "La categoria no puede ser vacia.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+                Archivo.guardar();
+    }//GEN-LAST:event_formWindowClosing
 
     /**
      * @param args the command line arguments
@@ -101,8 +163,11 @@ public class FrmPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTree treeCategorias;
+    private javax.swing.JTextField txtCategoria;
     // End of variables declaration//GEN-END:variables
 
       public void cargarCategorias()
