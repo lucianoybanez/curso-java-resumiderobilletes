7c5a428818f958df7c22a4f0d5dac2a0f9ed7d50
lucianoybanez <lucianoybanez@0f2bb4af-6c36-d3f4-e2bf-0f342f0e9b86>
2014-11-24 21:54:35 +0000

    Add Movimientos grid, and create Cuenta

---
+++ ResumideroBilletesUI/src/utn/resumiderobilletes/ui/modelos/ModeloMovimientos.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utn.resumiderobilletes.ui.modelos;
+
+
+import java.util.LinkedList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import utn.resumiderobilletes.entidades.Movimiento;
+import utn.resumiderobilletes.utils.DateUtil;
+
+/**
+ *
+ * @author Luciano Ybañez
+ */
+public class ModeloMovimientos extends AbstractTableModel
+{
+
+    private String[] columnas = {"Nº", "Fecha", "Monto", "Operación"};
+    private List<Movimiento> movimientos;
+
+    public ModeloMovimientos()
+    {
+        movimientos = new LinkedList<Movimiento>();
+    }
+
+    public ModeloMovimientos(List<Movimiento> lista)
+    {
+        movimientos = lista;
+    }
+
+    @Override
+    public int getRowCount()
+    {
+        int wRes = 25;
+        if (movimientos.size() > 25)
+        {
+            wRes = movimientos.size();
+        }
+        return wRes;
+    }
+
+    @Override
+    public int getColumnCount()
+    {
+        return 4;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex)
+    {
+        String res = "";
+        if (rowIndex < movimientos.size())
+        {
+            Movimiento aux = movimientos.get(rowIndex);
+            switch (columnIndex)
+            {
+                case 0:
+                    res = String.valueOf(aux.getNumero());
+                    break;
+                case 1:
+                    res = DateUtil.toStringDate(aux.getFechaMovimiento());
+                    break;
+                case 2:
+                    res = String.valueOf(aux.getMonto());
+                    break;
+                case 3:
+                    res = aux.isDeposito() ? "Deposito" : "Extracción";
+                    break;
+
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public String getColumnName(int column)
+    {
+        return columnas[column];
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex)
+    {
+        return false;
+    }
+
+    public Movimiento getItem(int pRowIndex)
+    {
+        Movimiento wResp = null;
+        if(pRowIndex < movimientos.size())
+            wResp = movimientos.get(pRowIndex);
+        return wResp;
+    }
+    
+    
+}
